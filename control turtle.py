4c0a535821c7750d266898fe1be4416fd2b4cf30
hndd0725 <hndd0725@naver.com>
2023-09-14 14:29:21 +0900

    Drill 1 제출 (2020180044 현대윤)

---
+++ control turtle.py
@@ -0,0 +1,35 @@
+import turtle
+
+def move_left():
+    turtle.stamp()
+    turtle.setheading(180)
+    turtle.forward(50)
+    
+def move_right():
+    turtle.stamp()
+    turtle.setheading(0)
+    turtle.forward(50)
+    
+    
+def move_up():
+    turtle.stamp()
+    turtle.setheading(90)
+    turtle.forward(50)
+    
+def move_down():
+    turtle.stamp()
+    turtle.setheading(270)
+    turtle.forward(50)
+    
+def restart():
+    turtle.reset()
+
+    
+turtle.shape('turtle')
+    
+turtle.onkey(restart,'Escape')
+turtle.onkey(move_left,'a')
+turtle.onkey(move_right,'d')
+turtle.onkey(move_up,'w')
+turtle.onkey(move_down,'s')
+turtle.listen()
